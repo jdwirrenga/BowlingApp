7db84d68815e82d6e2d5e23d51075d65768ca95a
Jaden Wirrenga <jdwirrenga@outlook.com>
2020-10-14 15:21:18 -0500

    First draft (working state)

---
+++ BowlingApp/BowlingApp/BowlingForm.Designer.cs
@@ -0,0 +1,366 @@
+﻿namespace BowlingApp
+{
+    partial class BowlingForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.FrameLabel = new System.Windows.Forms.Label();
+            this.ScoreButton = new System.Windows.Forms.Button();
+            this.scoreTextBox = new System.Windows.Forms.TextBox();
+            this.TotalScore = new System.Windows.Forms.Label();
+            this.TotalScoreLabel = new System.Windows.Forms.Label();
+            this.ErrorLabel = new System.Windows.Forms.Label();
+            this.labelFrame1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.f3Score = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.f7Score = new System.Windows.Forms.Label();
+            this.f8Score = new System.Windows.Forms.Label();
+            this.f10Score = new System.Windows.Forms.Label();
+            this.f9Score = new System.Windows.Forms.Label();
+            this.f4Score = new System.Windows.Forms.Label();
+            this.f5Score = new System.Windows.Forms.Label();
+            this.f6Score = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.f2Score = new System.Windows.Forms.Label();
+            this.f1Score = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 39F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(130, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(528, 70);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bowling Score Keeper";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // FrameLabel
+            // 
+            this.FrameLabel.AutoSize = true;
+            this.FrameLabel.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.FrameLabel.Location = new System.Drawing.Point(57, 186);
+            this.FrameLabel.Name = "FrameLabel";
+            this.FrameLabel.Size = new System.Drawing.Size(82, 28);
+            this.FrameLabel.TabIndex = 1;
+            this.FrameLabel.Text = "Frame 1";
+            // 
+            // ScoreButton
+            // 
+            this.ScoreButton.Location = new System.Drawing.Point(144, 217);
+            this.ScoreButton.Name = "ScoreButton";
+            this.ScoreButton.Size = new System.Drawing.Size(75, 23);
+            this.ScoreButton.TabIndex = 2;
+            this.ScoreButton.Text = "Score";
+            this.ScoreButton.UseVisualStyleBackColor = true;
+            this.ScoreButton.Click += new System.EventHandler(this.ScoreButton_Click);
+            // 
+            // scoreTextBox
+            // 
+            this.scoreTextBox.Location = new System.Drawing.Point(57, 217);
+            this.scoreTextBox.Name = "scoreTextBox";
+            this.scoreTextBox.Size = new System.Drawing.Size(81, 23);
+            this.scoreTextBox.TabIndex = 3;
+            // 
+            // TotalScore
+            // 
+            this.TotalScore.AutoSize = true;
+            this.TotalScore.Location = new System.Drawing.Point(59, 273);
+            this.TotalScore.Name = "TotalScore";
+            this.TotalScore.Size = new System.Drawing.Size(67, 15);
+            this.TotalScore.TabIndex = 4;
+            this.TotalScore.Text = "Total Score:";
+            // 
+            // TotalScoreLabel
+            // 
+            this.TotalScoreLabel.AutoSize = true;
+            this.TotalScoreLabel.Location = new System.Drawing.Point(135, 273);
+            this.TotalScoreLabel.Name = "TotalScoreLabel";
+            this.TotalScoreLabel.Size = new System.Drawing.Size(13, 15);
+            this.TotalScoreLabel.TabIndex = 5;
+            this.TotalScoreLabel.Text = "0";
+            // 
+            // ErrorLabel
+            // 
+            this.ErrorLabel.AutoSize = true;
+            this.ErrorLabel.ForeColor = System.Drawing.Color.Red;
+            this.ErrorLabel.Location = new System.Drawing.Point(60, 248);
+            this.ErrorLabel.Name = "ErrorLabel";
+            this.ErrorLabel.Size = new System.Drawing.Size(0, 15);
+            this.ErrorLabel.TabIndex = 6;
+            // 
+            // labelFrame1
+            // 
+            this.labelFrame1.AutoSize = true;
+            this.labelFrame1.Location = new System.Drawing.Point(63, 318);
+            this.labelFrame1.Name = "labelFrame1";
+            this.labelFrame1.Size = new System.Drawing.Size(49, 15);
+            this.labelFrame1.TabIndex = 7;
+            this.labelFrame1.Text = "Frame 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(130, 318);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 15);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Frame 2";
+            // 
+            // f3Score
+            // 
+            this.f3Score.AutoSize = true;
+            this.f3Score.Location = new System.Drawing.Point(196, 345);
+            this.f3Score.Name = "f3Score";
+            this.f3Score.Size = new System.Drawing.Size(0, 15);
+            this.f3Score.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(402, 318);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Frame 6";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(332, 318);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 15);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Frame 5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(264, 318);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(49, 15);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Frame 4";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(619, 318);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(49, 15);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Frame 9";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(691, 318);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(55, 15);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Frame 10";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(542, 318);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(49, 15);
+            this.label9.TabIndex = 7;
+            this.label9.Text = "Frame 8";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(469, 318);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(49, 15);
+            this.label10.TabIndex = 7;
+            this.label10.Text = "Frame 7";
+            // 
+            // f7Score
+            // 
+            this.f7Score.AutoSize = true;
+            this.f7Score.Location = new System.Drawing.Point(469, 345);
+            this.f7Score.Name = "f7Score";
+            this.f7Score.Size = new System.Drawing.Size(0, 15);
+            this.f7Score.TabIndex = 7;
+            // 
+            // f8Score
+            // 
+            this.f8Score.AutoSize = true;
+            this.f8Score.Location = new System.Drawing.Point(542, 345);
+            this.f8Score.Name = "f8Score";
+            this.f8Score.Size = new System.Drawing.Size(0, 15);
+            this.f8Score.TabIndex = 7;
+            // 
+            // f10Score
+            // 
+            this.f10Score.AutoSize = true;
+            this.f10Score.Location = new System.Drawing.Point(691, 345);
+            this.f10Score.Name = "f10Score";
+            this.f10Score.Size = new System.Drawing.Size(0, 15);
+            this.f10Score.TabIndex = 7;
+            // 
+            // f9Score
+            // 
+            this.f9Score.AutoSize = true;
+            this.f9Score.Location = new System.Drawing.Point(619, 345);
+            this.f9Score.Name = "f9Score";
+            this.f9Score.Size = new System.Drawing.Size(0, 15);
+            this.f9Score.TabIndex = 7;
+            // 
+            // f4Score
+            // 
+            this.f4Score.AutoSize = true;
+            this.f4Score.Location = new System.Drawing.Point(264, 345);
+            this.f4Score.Name = "f4Score";
+            this.f4Score.Size = new System.Drawing.Size(0, 15);
+            this.f4Score.TabIndex = 7;
+            // 
+            // f5Score
+            // 
+            this.f5Score.AutoSize = true;
+            this.f5Score.Location = new System.Drawing.Point(332, 345);
+            this.f5Score.Name = "f5Score";
+            this.f5Score.Size = new System.Drawing.Size(0, 15);
+            this.f5Score.TabIndex = 7;
+            // 
+            // f6Score
+            // 
+            this.f6Score.AutoSize = true;
+            this.f6Score.Location = new System.Drawing.Point(402, 345);
+            this.f6Score.Name = "f6Score";
+            this.f6Score.Size = new System.Drawing.Size(0, 15);
+            this.f6Score.TabIndex = 7;
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Location = new System.Drawing.Point(196, 318);
+            this.label18.Name = "label18";
+            this.label18.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label18.Size = new System.Drawing.Size(49, 15);
+            this.label18.TabIndex = 7;
+            this.label18.Text = "Frame 3";
+            // 
+            // f2Score
+            // 
+            this.f2Score.AutoSize = true;
+            this.f2Score.Location = new System.Drawing.Point(130, 345);
+            this.f2Score.Name = "f2Score";
+            this.f2Score.Size = new System.Drawing.Size(0, 15);
+            this.f2Score.TabIndex = 7;
+            // 
+            // f1Score
+            // 
+            this.f1Score.AutoSize = true;
+            this.f1Score.Location = new System.Drawing.Point(63, 345);
+            this.f1Score.Name = "f1Score";
+            this.f1Score.Size = new System.Drawing.Size(0, 15);
+            this.f1Score.TabIndex = 7;
+            // 
+            // BowlingForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.f1Score);
+            this.Controls.Add(this.f2Score);
+            this.Controls.Add(this.label18);
+            this.Controls.Add(this.f6Score);
+            this.Controls.Add(this.f5Score);
+            this.Controls.Add(this.f4Score);
+            this.Controls.Add(this.f9Score);
+            this.Controls.Add(this.f10Score);
+            this.Controls.Add(this.f8Score);
+            this.Controls.Add(this.f7Score);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.f3Score);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelFrame1);
+            this.Controls.Add(this.ErrorLabel);
+            this.Controls.Add(this.TotalScoreLabel);
+            this.Controls.Add(this.TotalScore);
+            this.Controls.Add(this.scoreTextBox);
+            this.Controls.Add(this.ScoreButton);
+            this.Controls.Add(this.FrameLabel);
+            this.Controls.Add(this.label1);
+            this.Name = "BowlingForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label FrameLabel;
+        private System.Windows.Forms.Button ScoreButton;
+        private System.Windows.Forms.TextBox scoreTextBox;
+        private System.Windows.Forms.Label TotalScore;
+        private System.Windows.Forms.Label TotalScoreLabel;
+        private System.Windows.Forms.Label ErrorLabel;
+        private System.Windows.Forms.Label labelFrame1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label f3Score;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label f7Score;
+        private System.Windows.Forms.Label f8Score;
+        private System.Windows.Forms.Label f10Score;
+        private System.Windows.Forms.Label f9Score;
+        private System.Windows.Forms.Label f4Score;
+        private System.Windows.Forms.Label f5Score;
+        private System.Windows.Forms.Label f6Score;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.Label f2Score;
+        private System.Windows.Forms.Label f1Score;
+    }
+}
+
---
+++ BowlingApp/BowlingApp/BowlingForm.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BowlingApp
+{
+    public partial class BowlingForm : Form
+    {
+        int currentFrame;
+        bool[] spareFrames;
+        bool[] strikeFrames;
+        Dictionary<int, List<int>> frameScores;
+        
+        public BowlingForm()
+        {
+            InitializeComponent();
+
+            currentFrame = 1;
+            spareFrames = new bool[11];
+            strikeFrames = new bool[11];
+            frameScores = new Dictionary<int, List<int>>();
+
+            FrameLabel.Text = $"Frame {currentFrame}";
+        }
+
+        private void ScoreButton_Click(object sender, EventArgs e)
+        {
+            if (!frameScores.ContainsKey(currentFrame)) frameScores.Add(currentFrame, new List<int>());
+
+            if(NewScoreIsValid())
+            {
+                int newScore = int.Parse(scoreTextBox.Text);
+                frameScores[currentFrame].Add(newScore);
+
+                UpdatePreviousStrikesAndSpares(newScore);
+                CheckStrikeOrSpare(newScore);
+
+                if (currentFrame < 10)
+                {
+                    if (strikeFrames[currentFrame] || frameScores[currentFrame].Count == 2)
+                    {
+                        NewFrame();
+                    }
+                }
+                else
+                {
+                    if (frameScores[currentFrame].Count == 2 && !strikeFrames[currentFrame] && !spareFrames[currentFrame])
+                    {
+                        CompleteGame();
+                    }
+                    else if(frameScores[currentFrame].Count == 3)
+                    {
+                        CompleteGame();
+                    }
+                }
+    
+                ErrorLabel.Text = null;
+            }
+
+            scoreTextBox.Text = null;
+        }
+
+        private bool NewScoreIsValid()
+        {
+            int newScore = -1;
+
+            try
+            {
+                newScore = int.Parse(scoreTextBox.Text);
+            }
+            catch (Exception)
+            {
+                ErrorLabel.Text = "Invalid input, please enter a number between 0 and 10";
+            }
+
+            if (newScore < 0 || newScore > 10)
+            {
+                ErrorLabel.Text = "Invalid input, please enter a number between 0 and 10";
+                return false;
+            }
+            else
+            {
+                int thisFrameScore = frameScores[currentFrame].Sum();
+                if (currentFrame < 10)
+                {
+                    if (thisFrameScore + newScore > 10)
+                    {
+                        ErrorLabel.Text = "Invalid input, can't score over 10 in one frame!";
+                        return false;
+                    }
+                }
+                else
+                {
+                    if(thisFrameScore + newScore > 10 && frameScores[currentFrame].Count == 1 && !strikeFrames[currentFrame])
+                    {
+                        ErrorLabel.Text = "Invalid input, can't score over 10 without a strike first!";
+                        return false;
+                    }
+                    else if(frameScores[currentFrame].Count == 2 && !spareFrames[currentFrame])
+                    {
+                        int lastBowl = frameScores[currentFrame].Last();
+                        if (lastBowl != 10 && lastBowl + newScore > 10)
+                        {
+                            ErrorLabel.Text = "Invalid input, can't score over 10!";
+                            return false;
+                        }
+                    }
+                } 
+            }
+
+            return true;
+        }
+
+        private void NewFrame()
+        {
+            int totalScore = CalculateScores();
+            currentFrame++;
+
+            FrameLabel.Text = $"Frame {currentFrame}";
+            TotalScoreLabel.Text = totalScore.ToString();
+        }
+
+        private void CheckStrikeOrSpare(int newScore)
+        {
+            if (newScore == 10) //strike
+            {
+                strikeFrames[currentFrame] = true;
+            }
+            else if (frameScores[currentFrame].Count == 2)
+            {
+                if (frameScores[currentFrame].Sum() == 10) //spare
+                {
+                    spareFrames[currentFrame] = true;
+                }
+            }
+        }
+        private void UpdatePreviousStrikesAndSpares(int newScore)
+        {
+            if (currentFrame > 1 && strikeFrames[currentFrame - 1] && frameScores[currentFrame].Count < 3)
+            {
+                frameScores[currentFrame - 1].Add(newScore);
+
+                if ((currentFrame > 2 && strikeFrames[currentFrame - 2]) && frameScores[currentFrame].Count < 2) 
+                    frameScores[currentFrame - 2].Add(newScore);
+            }
+
+            if (currentFrame > 1 && spareFrames[currentFrame - 1] && frameScores[currentFrame].Count == 1) 
+                frameScores[currentFrame - 1].Add(newScore);
+        }
+
+        private int CalculateScores()
+        {
+            f1Score.Text = frameScores.ContainsKey(1) ? frameScores[1].Sum().ToString() : null;
+            f2Score.Text = frameScores.ContainsKey(2) ? (frameScores[2].Sum() + int.Parse(f1Score.Text)).ToString() : null;
+            f3Score.Text = frameScores.ContainsKey(3) ? (frameScores[3].Sum() + int.Parse(f2Score.Text)).ToString() : null;
+            f4Score.Text = frameScores.ContainsKey(4) ? (frameScores[4].Sum() + int.Parse(f3Score.Text)).ToString() : null;
+            f5Score.Text = frameScores.ContainsKey(5) ? (frameScores[5].Sum() + int.Parse(f4Score.Text)).ToString() : null;
+            f6Score.Text = frameScores.ContainsKey(6) ? (frameScores[6].Sum() + int.Parse(f5Score.Text)).ToString() : null;
+            f7Score.Text = frameScores.ContainsKey(7) ? (frameScores[7].Sum() + int.Parse(f6Score.Text)).ToString() : null;
+            f8Score.Text = frameScores.ContainsKey(8) ? (frameScores[8].Sum() + int.Parse(f7Score.Text)).ToString() : null;
+            f9Score.Text = frameScores.ContainsKey(9) ? (frameScores[9].Sum() + int.Parse(f8Score.Text)).ToString() : null;
+            f10Score.Text = frameScores.ContainsKey(10) ? (frameScores[10].Sum() + int.Parse(f9Score.Text)).ToString() : null;
+
+            int score = 0;
+            foreach(List<int> scores in frameScores.Values)
+            {
+                score += scores.Sum();
+            }
+
+            return score;
+        }
+
+        private void CompleteGame()
+        {
+            int totalScore = CalculateScores();
+
+            scoreTextBox.Visible = false;
+            ScoreButton.Visible = false;
+            FrameLabel.Text = $"Game Complete!";
+            TotalScoreLabel.Text = totalScore.ToString();
+        }
+    }
+}
